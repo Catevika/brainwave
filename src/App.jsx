9aeca50605fa79f753c8b608c7ee4b3f72b56db8
Catevika <dominique.bello@outlook.fr>
2026-09-14 19:43:20 +0200

    Update packages

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,21 @@ import Services from './components/Services';
 
 const App = () => {
   return (
-    <>
-      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
-        <Header />
-        <Hero />
-        <Benefits />
-        <Collaboration />
-        <Services />
-        <Pricing />
-        <Roadmap />
-        <Footer />
-      </div>
+		<>
+			<div className="pt-19 lg:pt-21 overflow-hidden">
+				<Header />
+				<Hero />
+				<Benefits />
+				<Collaboration />
+				<Services />
+				<Pricing />
+				<Roadmap />
+				<Footer />
+			</div>
 
-      <ButtonGradient />
-    </>
-  );
+			<ButtonGradient />
+		</>
+	);
 };
 
 export default App;
